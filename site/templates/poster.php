f3bdea2fae27d9508f444a7c2ba23e519bb0153f
pellenberger <patrick.ellenberger@ik.me>
2026-01-17 15:15:39 +0100

    Remove kirby-color-palette plugin
    gh-6

--- site/templates/poster.php
+++ site/templates/poster.php
@@ -12,56 +12,55 @@
 
 <body>
 
-<?php  $color = $page->palette()->yaml() ?>
-  <div id='wrapper' style="background:<?php echo $color[0] ?>; color: <?php e($color[0] === "rgb(231,144,136)", "rgb(1, 55, 93)", "white"); ?>" >
+<div id='wrapper' style="background:rgb(26,41,102); color: white;" >
 
-    <header>
-      <h3>Café Littéraire</h3>
-      <h2>Programme</h2>
-    </header>
-    <main class="flex justify-content-center align-item-center">
-      <div class="grid-col-90 text-center inner">
-        <?php foreach ($page->events()->toStructure() as $items): ?>
-          <div>
-            <h4><?php echo $items->date(); ?></h4>
-            <h3><?php echo $items->title(); ?></h3>
-            <h4><?php echo $items->subtitle(); ?></h4>
-          </div>
-        <?php endforeach ?>
-      </div>
-
-    </main>
-
-
-    <sidebar>
-      <div>
-        <h1><?php echo $page->month() ?></h1>
-      </div>
-    </sidebar>
-
-    <footer class="width-100">
-    <div class="width-100 text-center">Retrouvez notre programme complet sur le site web www.litcafe.ch</div>
-    <div class="flex  width-100 justify-content-spacearound time-table">
-      <div class="grid-col-90 flex justify-content-spacebetween ">   
-        <ul>
-          <li><h4>Literaturcafé</h4></li>
-          <li>Rue Haute 11</lir>
-          <li>2502 Biel/Bienne</li>
-          <li>www.litcafe.ch</li>
-          <li id="socila-media-icon">facebook | instagram</li>
-        </ul>
-        <ul>
-          <li><h4>Dienstag</h4></li>
-          <li>19:30 MardiPhilo</li>
-          <li><h4>Vendredi</h4></li>
-          <li>20:00 Konzert</li>
-        </ul>
-      </div>
+  <header>
+    <h3>Café Littéraire</h3>
+    <h2>Programme</h2>
+  </header>
+  <main class="flex justify-content-center align-item-center">
+    <div class="grid-col-90 text-center inner">
+      <?php foreach ($page->events()->toStructure() as $items): ?>
+        <div>
+          <h4><?php echo $items->date(); ?></h4>
+          <h3><?php echo $items->title(); ?></h3>
+          <h4><?php echo $items->subtitle(); ?></h4>
+        </div>
+      <?php endforeach ?>
     </div>
-    <div class="width-100 text-center">Mit freundlicher Unterstützung der Stadt Biel und SWISSLOS/Kultur Kanton Bern</div>
-    </footer>
-    
+
+  </main>
+
+
+  <sidebar>
+    <div>
+      <h1><?php echo $page->month() ?></h1>
+    </div>
+  </sidebar>
+
+  <footer class="width-100">
+  <div class="width-100 text-center">Retrouvez notre programme complet sur le site web www.litcafe.ch</div>
+  <div class="flex  width-100 justify-content-spacearound time-table">
+    <div class="grid-col-90 flex justify-content-spacebetween ">   
+      <ul>
+        <li><h4>Literaturcafé</h4></li>
+        <li>Rue Haute 11</lir>
+        <li>2502 Biel/Bienne</li>
+        <li>www.litcafe.ch</li>
+        <li id="socila-media-icon">facebook | instagram</li>
+      </ul>
+      <ul>
+        <li><h4>Dienstag</h4></li>
+        <li>19:30 MardiPhilo</li>
+        <li><h4>Vendredi</h4></li>
+        <li>20:00 Konzert</li>
+      </ul>
+    </div>
   </div>
+  <div class="width-100 text-center">Mit freundlicher Unterstützung der Stadt Biel und SWISSLOS/Kultur Kanton Bern</div>
+  </footer>
+  
+</div>
       
 
 
